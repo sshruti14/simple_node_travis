a9ea539ab61d87c0400ab19061dd5215a0b9ea8f
Shruti Shetty <shetty_14august@yahoo.com>
2020-11-08 23:09:49 -0500

    Adding an environment variable test

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+const FAVOURITE_INSTRUCTOR = process.env.INSTRUCTOR_NAME ;
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`${FAVOURITE_INSTRUCTOR}`);
     await sleep(5000);
   }
 }
